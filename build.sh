bced22af01c01a40d973100382a07c46f8df875e
Anurag Goel <anurag@render.com>
2019-04-12 16:16:32 -0700

    Add meteor to path

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-export HOME=`pwd`
 curl https://install.meteor.com/ | sh
-npm install
+yarn install
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-export HOME=`pwd`
 export PATH="$HOME/.meteor:$PATH"
-npm run start
+yarn start
