b271b45032d06623e234cc257c12c805e9a94fa1
Anurag Goel <anurag@render.com>
2019-04-12 16:56:05 -0700

    Update home for render

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+export HOME=$RENDER_PROJECT_ROOT
 curl https://install.meteor.com/ | sh
 yarn install
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+export HOME=$RENDER_PROJECT_ROOT;
 export PATH="$HOME/.meteor:$PATH"
 yarn start
