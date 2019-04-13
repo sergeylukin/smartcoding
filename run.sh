88032402158feb3841a5f761ef9e959b45323bf5
Sergey Lukin <contact@sergeylukin.com>
2019-04-13 03:39:19 +0300

    Add remote MongoDB

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ export HOME=$RENDER_PROJECT_ROOT;
 export PATH="$HOME/.meteor:$PATH"
 export ROOT_URL=https://smartcoding.onrender.com
 export NODE_ENV=production
+export MONGO_URL=mongodb+srv://sergey:Passw0rd!@cluster0-8v5wp.mongodb.net/test?retryWrites=true
 yarn start
