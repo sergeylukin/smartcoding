014977d138b8f1bda857082fa11b8e655429ccdf
Sergey Lukin <contact@sergeylukin.com>
2019-04-13 03:26:57 +0300

    Set production mode

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 export HOME=$RENDER_PROJECT_ROOT;
 export PATH="$HOME/.meteor:$PATH"
 export ROOT_URL=https://smartcoding.onrender.com
-#export NODE_ENV=production
+export NODE_ENV=production
 yarn start
